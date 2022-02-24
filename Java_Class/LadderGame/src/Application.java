0ef6a1b610a5844aa543d8c76f40a3e75b80e4da
honeySleepr <92678400+honeySleepr@users.noreply.github.com>
2022-02-24 14:20:10 +0900

    Revert "사다리게임 PR 테스트"

--- Java_Class/LadderGame/src/Application.java
+++
@@ -1,9 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        Input input = new Input();
-        LadderMaker maker = new LadderMaker();
-        Output output = new Output();
-        Controller controller = new Controller(input, maker, output);
-        controller.runApp();
-    }
-}
--- Java_Class/LadderGame/src/Output.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Output {
-    public void printConsole(String[][] ladder) {
-        Arrays.stream(ladder)
-            .map(arr -> String.join("", arr))
-            .forEach(System.out::println);
-    }
-}
